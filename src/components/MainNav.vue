9e2852565d4b61a7ab8fb1552b2b9dfc57e90500
cristian <dumitriucristian@yahoo.com>
2022-07-28 10:38:51 +0300

    add data to component as a function object

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,10 +1,23 @@
 <template>
-  <header class="w-full text-sm">MainNav</header>
+  <header class="w-full text-sm">
+    <div class="fixed top-0 left-0 w-full bg-white">
+      <div
+        class="flex flex-nowrap h-full px-8 mx-auto border-b bored-solid border-brand-gray-1"
+      >
+        <a href="/" class="flex items-center h-full text-xl">{{ someText }}</a>
+      </div>
+    </div>
+  </header>
 </template>
 <script>
 //import { resolveComponents } from "uri-js";
 
 export default {
   name: "MainNav",
+  data() {
+    return {
+      someText: "This is some text",
+    };
+  },
 };
 </script>
