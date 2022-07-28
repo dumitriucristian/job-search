f0c47043e9a517b0cd84ad29efd26b288b896eb6
cristian <dumitriucristian@yahoo.com>
2022-07-28 09:28:34 +0300

    fix eslint

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <h1 class="mt-8 text-6xl text-center">Hello Vue</h1>
+  <MainNav></MainNav>
 </template>
 
 <script>
+import MainNav from "./components/MainNav.vue";
+
 export default {
   name: "App",
+  components: {
+    MainNav,
+  },
 };
 </script>
---
+++ src/components/MainNav.vue
@@ -0,0 +1,10 @@
+<template>
+  <header class="w-full text-sm">MainNav</header>
+</template>
+<script>
+//import { resolveComponents } from "uri-js";
+
+export default {
+  name: "MainNav",
+};
+</script>
