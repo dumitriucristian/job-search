98407e8dae5dd96d7e4c0ad8012ad331a35fdb59
cristian <dumitriucristian@yahoo.com>
2022-07-28 11:35:02 +0300

    create jest test with nested describe

---
+++ src/playground.js
@@ -0,0 +1,7 @@
+export const evenOrOdd = (number) => {
+  if (number % 2 === 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
+};
--- src/playground.test.js
+++ src/playground.test.js
@@ -1,3 +1,5 @@
+import { evenOrOdd } from "@/playground";
+
 describe("testing basic math", () => {
   it("cand add two numbers", () => {
     expect(2 + 2).toBe(4);
@@ -5,5 +7,19 @@ describe("testing basic math", () => {
 
   it("is able to substract two numbers", () => {
     expect(5 - 3).toBe(2);
+  });
+});
+
+describe("evenOrOdd", () => {
+  describe("when the number is even", () => {
+    it("expect to be even", () => {
+      expect(evenOrOdd(4)).toBe("even");
+    });
+  });
+
+  describe("when the number is odd", () => {
+    it("expect to be odd", () => {
+      expect(evenOrOdd(3)).toBe("odd");
+    });
   });
 });
