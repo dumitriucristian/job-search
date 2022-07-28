b508247d236233fa0184721701682dbfa3935b61
cristian <dumitriucristian@yahoo.com>
2022-07-28 16:21:27 +0300

    create a nav

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,12 +2,28 @@
   <header class="w-full text-sm">
     <div class="fixed top-0 left-0 w-full bg-white">
       <div
-        class="flex flex-nowrap h-full px-8 mx-auto border-b bored-solid border-brand-gray-1"
+        class="flex flex-nowrap h-full px-8 mx-auto border-b bored-solid border-brand-gray-1 items-center"
       >
         <a :href="url" class="flex items-center h-full text-xl">{{ company }}</a>
-        <h2 class="flex items-center h-full ml-8">
-          Developed by {{ author.firstName }} {{ author.lastName }}
-        </h2>
+        <nav class="h-full ml-12">
+          <ul class="flex w-full h-full p-0 m-0 list-none">
+            <li class="h-full">
+              <a href="#" class="flex items-center h-full py-2.5">Bobo Corp</a>
+            </li>
+            <li class="h-full ml-9">
+              <a href="#" class="flex items-center h-full py-2.5">What we do</a>
+            </li>
+            <li class="h-full ml-9">
+              <a href="#" class="flex items-center h-full py-2.5">Who we are</a>
+            </li>
+            <li class="h-full ml-9">
+              <a href="#" class="flex items-center h-full py-2.5">Teams</a>
+            </li>
+            <li class="h-full ml-9">
+              <a href="#" class="flex items-center h-full py-2.5">Locations</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
