6dfa25eecf346d0b9b95abbe8346ebd0cc17b8a9
cristian <dumitriucristian@yahoo.com>
2022-07-28 11:13:42 +0300

    use jest for test

---
+++ src/playground.test.js
@@ -0,0 +1,9 @@
+describe("testing basic math", () => {
+  it("cand add two numbers", () => {
+    expect(2 + 2).toBe(4);
+  });
+
+  it("is able to substract two numbers", () => {
+    expect(5 - 3).toBe(2);
+  });
+});
