7591fa4b6a3c15550df4bc9a1d6ca5c14a359eba
cristian <dumitriucristian@yahoo.com>
2022-08-01 16:25:45 +0300

    apply custom css

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,12 +1,16 @@
 <template>
-  <button
-    class="bg-brand-blue-1 px-5 py-3 rounded text-white font-medium border-0 hover:shadow-blue"
-  >
-    Sign In
-  </button>
+  <button class="primary">Sign In</button>
 </template>
 <script>
 export default {
   name: "ActionButton",
 };
 </script>
+<style scoped>
+button {
+  @apply px-5 py-3 font-medium;
+}
+.primary {
+  @apply bg-brand-blue-1 rounded text-white;
+}
+</style>
