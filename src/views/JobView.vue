1c2afbdff77c2f5c4e3c0b037e048d614b250641
cristian <dumitriucristian@yahoo.com>
2022-08-10 14:42:12 +0300

    create dynamic url params for routes

---
+++ src/views/JobView.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>This is the job view {{ jobId }}</h1>
+</template>
+<script>
+export default {
+  name: "JobView",
+  computed: {
+    jobId() {
+      return this.$route.params.id;
+    },
+  },
+};
+</script>
